9abf67e13624983c33b054d936056c8956e237a5
Jon Marco Reyes <rjonmarco@gmail.com>
2018-06-23 18:35:32 +0800

    Power Analyzer Code
    Power Analyzer is set to Mode 4

--- PowerSocket/PowerSocket.ino
+++ PowerSocket/PowerSocket.ino
@@ -1,17 +1,63 @@
+#include <SoftwareSerial.h>
+#include <SerialESP8266wifi.h>
 
-int relayPin = 2;
-int proximityPin =0;
+#define relayPin 12
+#define proximityPin 0
+#define poweranalyzer_rx 3
+#define poweranalyzer_tx 2
+
+SoftwareSerial poweranalyzer(poweranalyzer_rx, poweranalyzer_tx);
+String Data;
+String watthr;
+String volt;
+String amp;
+String power;
+String acs;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+
+  //Serial Setup
+  // Different Serial Processes needs to have different baud rate to be recognized
+  poweranalyzer.begin(9600);
+  Serial.begin(19200);
+  
+  poweranalyzer.listen();
+  poweranalyzer.print("\002M4\003"); //“\002”=STX, “\003”=ETX
+  
   pinMode(relayPin, OUTPUT);
-  pinMode(relayPin,INPUT);
+  pinMode(relayPin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  isPluggedin();
+  poweranalyzerfunc();
+}
+
+void poweranalyzerfunc(){
+    if (poweranalyzer.available()>0) {
+     if (poweranalyzer.find("Volt")){
+      volt = poweranalyzer.parseFloat();
+      Serial.print("Voltage: ");
+      Serial.println(volt);
+    }
+       if (poweranalyzer.find("Amp")){
+      amp = poweranalyzer.parseFloat();
+      Serial.print("Current: ");
+      Serial.println(amp);
+    }
+       if (poweranalyzer.find("Watt")){
+      power = poweranalyzer.parseFloat();
+      Serial.print("Current: ");
+      Serial.println(power);
+    }
+     if (poweranalyzer.find("Watt-Hr")){
+      watthr = poweranalyzer.parseFloat();
+      Serial.print("Watt Hours: ");
+      Serial.println(watthr);
+      //sendpowerdata();
+    }
+  }
 }
 
 void isPluggedin(){
@@ -21,7 +67,6 @@ void isPluggedin(){
   else if (proximitySensor() < 20){
     Serial.println("Appliance Plugged IN");
   }
-  
 }
 
 int proximitySensor(){
