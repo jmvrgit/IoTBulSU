69d89c294c0857dc5d69a2894d5986183a6411fa
Jon Marco Reyes <rjonmarco@gmail.com>
2018-07-01 16:37:04 +0800

    MFRC522 RFID Interfaced

--- PowerSocket/PowerSocket.ino
+++ PowerSocket/PowerSocket.ino
@@ -1,18 +1,27 @@
+#include <deprecated.h>
+#include <MFRC522.h>
+#include <MFRC522Extended.h>
+#include <require_cpp11.h>
 #include <SoftwareSerial.h>
-#include <SerialESP8266wifi.h>
 
 #define relayPin 12
 #define proximityPin 0
 #define poweranalyzer_rx 3
 #define poweranalyzer_tx 2
 
+MFRC522 mfrc522(10, 9);
 SoftwareSerial poweranalyzer(poweranalyzer_rx, poweranalyzer_tx);
+
 String Data;
 String watthr;
 String volt;
 String amp;
 String power;
 String acs;
+
+String UID_card;
+String stringTemp;
+byte readCard[4];
 
 void setup() {
   // put your setup code here, to run once:
@@ -27,11 +36,36 @@ void setup() {
   
   pinMode(relayPin, OUTPUT);
   pinMode(proximityPin, INPUT);
+
+  SPI.begin();
+  mfrc522.PCD_Init(); //Initialize MFRC522 Hardware
+  //mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  poweranalyzerfunc();
+
+  getID();
+}
+
+int getID() {
+  if ( ! mfrc522.PICC_IsNewCardPresent()) { //If a new PICC placed to RFID reader continues
+    return 0;
+  }
+  if ( ! mfrc522.PICC_ReadCardSerial()) {   //Since a PICC placed get Serial and continue
+    return 0;
+  }
+  // There are Mifare PICCs which have 4 byte or 7 byte UID care if you use 7 byte PICC
+  // I think we should assume every PICC as they have 4 byte UID
+  // Until we support 7 byte PICCs
+  UID_card = "";
+  for (int i = 0; i < 4; i++) {  //
+    readCard[i] = mfrc522.uid.uidByte[i];
+    stringTemp = String(readCard[i],HEX);
+    UID_card = UID_card + stringTemp;
+  }
+  mfrc522.PICC_HaltA(); // Stop reading
+  Serial.println(UID_card);
 }
 
 void poweranalyzerfunc(){
@@ -63,9 +97,11 @@ void poweranalyzerfunc(){
 void isPluggedin(){
   if(proximitySensor() > 200){
     Serial.println("No Appliance");
+    relayOff();
   }
   else if (proximitySensor() < 20){
     Serial.println("Appliance Plugged IN");
+    relayOn();
   }
 }
 
