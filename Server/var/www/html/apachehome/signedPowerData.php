d4fef45e741cf8a2aa215ad1a1ce605975be347b
Jon Marco Reyes <rjonmarco@gmail.com>
2018-08-25 18:34:09 +0800

    Combined getUID PHP and PowerData PHP to get signed powerdata php

---
+++ Server/var/www/html/apachehome/signedPowerData.php
@@ -0,0 +1,24 @@
+<html>
+	<body>
+		<?php
+		$UID = $_GET["UID"];
+		$powerdata = $_GET["powerdata"];
+		
+		$date = date("c");
+		
+		$AR_powerdata = explode( "||", $powerdata);
+		$voltage =$AR_powerdata[0];
+		$ampere =$AR_powerdata[1];
+		$power =$AR_powerdata[2];
+		$watthr =$AR_powerdata[3];
+		$date = date("c");
+		$status = file_put_contents("signedpowerfile.txt",$UID. "||" . $powerdata . "||" . $date . "\r\n" , FILE_APPEND);
+		if ($status){
+			echo "successful";
+		}
+		else {
+			echo "failed";
+		}
+		?>
+	<body>
+</html>
